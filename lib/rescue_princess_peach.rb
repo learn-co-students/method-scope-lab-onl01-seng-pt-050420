8d78069f0f2595afa947e4993196cccb66fbaa7c
Valentina Panic <valentina.panic87@gmail.com>
2020-05-07 00:12:34 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -2,8 +2,4 @@ def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
-
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
+# Variable inside the method cannot be accessed outside of the method
